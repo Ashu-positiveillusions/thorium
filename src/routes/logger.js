47ad3e6a4292be5ae985075ccfd9722250b83a34
Ashu-positiveillusions <ashutosh.meddy@gmail.com>
2022-02-21 16:06:52 +0530

    Hello!first time for everything.

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+let url = "www.google.com";
+
+function log(message){
+    console.log(message);
+}
+
+module.exports.url = url;
+module.exports.printMessage = log;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,10 @@
+const obj = require('./logger')
 const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
+    obj.printMessage("Trying my luck!");
+    console.log(obj.url);
     res.send('My first ever api!')
 });
 

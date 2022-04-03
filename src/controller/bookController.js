e0d35f65e4b75e5f966b6e3175d1f78aadc42d2b
Ashu-positiveillusions <ashutosh.meddy@gmail.com>
2022-04-03 07:34:40 +0530

    1

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -5,47 +5,7 @@ const validator=require("../validator/validator")
 const moment = require('moment')
 const { get } = require("../routes/route");
 const removeUploadedFiles = require('multer/lib/remove-uploaded-files');
-const aws = require("aws-sdk")
 
-
-
-aws.config.update(
-    {
-        accessKeyId: "AKIAY3L35MCRVFM24Q7U",
-        secretAccessKeyId: "qGG1HE0qRixcW1T1Wg1bv+08tQrIkFVyDFqSft4J",
-        region: "ap-south-1"
-    }
-)
-
-let uploadFile = async (file) => {
-    return await new Promise( function(resolve, reject) {
-        //this function will upload file to aws and return the link
-        let s3 = new aws.S3({ apiVersion: "2006-03-01" }) //we will be using s3 service of aws
-        uploadFile(files[0])
-        var uploadParams = {
-            ACL: "public-read",
-            Bucket: "classroom-training-bucket", // HERE
-            Key: "ashutosh/" + file.originalname, // HERE "ashutosh/smiley.jpg"
-            Body: file.buffer
-        }
-
-    s3.upload(uploadParams, function (err, data) {
-            if (err) { 
-                return reject({ "error": err }) 
-            }
-
-            console.log(data)
-            console.log(" file uploaded succesfully ")
-            return resolve(data.Location) // HERE
-    })
-
-    // let data= await s3.upload(uploadParams)
-    // if (data) return data.Location
-    // else return "there is an error"
-
-    }
-    )
-}
 
 
 const createBooks=async function(req,res){
@@ -111,30 +71,6 @@ try{
     // date validation for releasedAt
     let validity = moment(releasedAt, "YYYY-MM-DD",true).isValid();
     if(!validity) return res.status(400).send({status:false,message:"input a valid date in YYYY-MM-DD format."})
-
-
-
-
-    // AWS S3 Link Work
-
-
-    let files = req.files
-    if (files && files.length > 0) {
-        //upload to s3 and get the uploaded link
-        // res.send the link back to frontend/postman
-        let uploadedFileURL = await uploadFile(files[0])
-        bookData.bookCover = uploadedFileURL;
-        // res.status(201).send({ msg: "file uploaded succesfully", data: uploadedFileURL })
-    }
-    else {
-        return res.status(400).send({ msg: "No file found" })
-    }
-    
-
-
-
-
-
 
 
     let book=await BooksModel.create(bookData);

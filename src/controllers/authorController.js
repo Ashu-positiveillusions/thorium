320b3c4d86b64ad79df2e6e869ef6a2b60b93918
Ashu-positiveillusions <ashutosh.meddy@gmail.com>
2022-04-06 16:32:29 +0530

    redis working code

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -5,19 +5,17 @@ const { promisify } = require("util");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  10337,
-  "redis-10337.c264.ap-south-1-1.ec2.cloud.redislabs.com",
+  18800,
+  "redis-18800.c264.ap-south-1-1.ec2.cloud.redislabs.com",
   { no_ready_check: true }
 );
-redisClient.auth("6L8SXmAlBLG8Z55VsrQtoTd1ugZZ6Qci", function (err) {
+redisClient.auth("FGpuu4MNu9eKD5N1RwraHBXr0DG5fWU2", function (err) {
   if (err) throw err;
 });
 
 redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
-
-
 
 //1. connect to the server
 //2. use the commands :
